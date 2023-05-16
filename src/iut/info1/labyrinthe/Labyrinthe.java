3ed9f3a01236b7e23d939f8386bcbf740e7ebbd0
Briot Aurore <nael.briot@iut-rodez.fr>
2023-05-16 16:15:17 +0200

    Début du Labyrinthe et des tests

--- src/iut/info1/labyrinthe/Labyrinthe.java
+++ src/iut/info1/labyrinthe/Labyrinthe.java
@@ -1,43 +1,73 @@
 package iut.info1.labyrinthe;
 
+/** TODO comment class responsibility (SRP)
+ * @author nael.briot
+ *
+ */
 public class Labyrinthe {
-	
-	private int nbColonnes;
-	private int nbLignes;
-	private int[][] tableau;
 
-	public Labyrinthe(int lignes, int colonnes) {
-		if (!isValide(lignes, colonnes)) {
-			throw new IllegalArgumentException();
-		}
-	}
-	
-	private static boolean isValide(int lignes, int colonnes) {
-		return lignes >= 0;
-	}
+    private int nbColonnes;
+    private int nbLignes;
+    private int[][] tableau;
 
-	@Override
-	public int hashCode() {
-		// TODO Auto-generated method stub
-		return super.hashCode();
-	}
+    /**
+     * Crée un labyrinthe en fonction d'un nombre de salles en longueur
+     * et en hauteur.
+     * @param lignes
+     * @param colonnes
+     */
+    public Labyrinthe(int lignes, int colonnes) {
+        if (!isValide(lignes, colonnes)) {
+            throw new IllegalArgumentException();
+        }
+        this.nbColonnes = colonnes;
+        this.nbLignes = lignes;
+        this.tableau = new int[nbLignes][nbColonnes];
+        
+        int numeroCase = 1;
 
-	@Override
-	public String toString() {
-		// TODO Auto-generated method stub
-		return super.toString();
-	}
+        for (int ligne = 0 ; ligne < tableau.length ; ligne++) {
+            for (int colonne = 0 ; colonne < tableau[ligne].length ; colonne++) {
+                tableau[ligne][colonne] = numeroCase;
+                numeroCase++;
+            }
+        }
+    }
 
-	public int getNbColonnes() {
-		return nbColonnes;
-	}
+    private static boolean isValide(int lignes, int colonnes) {
+        return lignes >= 3 && colonnes >= 3;
+    }
 
-	public int getNbLignes() {
-		return nbLignes;
-	}
+    @Override
+    public int hashCode() {
+        return this.tableau.hashCode() / 100 + this.nbColonnes * 10 + this.nbLignes;
+    }
 
-	public int[][] getTableau() {
-		return tableau;
-	}
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return super.toString();
+    }
+
+    /**
+     * @return le nombre de colonnes
+     */
+    public int getNbColonnes() {
+        return nbColonnes;
+    }
+
+    /**
+     * @return le nombre de lignes
+     */
+    public int getNbLignes() {
+        return nbLignes;
+    }
+
+    /**
+     * @return le tableau
+     */
+    public int[][] getTableau() {
+        return tableau;
+    }
 
 }
--- src/iut/info1/labyrinthe/tests/TestLabyrinthe.java
+++ src/iut/info1/labyrinthe/tests/TestLabyrinthe.java
@@ -1,6 +1,11 @@
 package iut.info1.labyrinthe.tests;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -9,39 +14,59 @@ import iut.info1.labyrinthe.Labyrinthe;
 
 class TestLabyrinthe {
 
-	@BeforeEach
-	void setUp() throws Exception {
-		Labyrinthe labtest = new Labyrinthe(3, 5);
-	}
-		
-	@Test
-	void testLabyrinthe() {
-		assertThrows(IllegalArgumentException.class,() -> new Labyrinthe(-8, 5));
-	}
+    List<Labyrinthe> correctes = new ArrayList<Labyrinthe>();
 
-	@Test
-	void testToString() {
-		
-	}
+    @BeforeEach
+    void setUp() throws Exception {
+        correctes.clear();
+        correctes.add( new Labyrinthe(3, 5));
+        correctes.add( new Labyrinthe(8, 8));
+    }
 
-	@Test
-	void testGetNbColonnes() {
-		
-	}
+    @Test
+    void testLabyrinthe() {
+        assertThrows(IllegalArgumentException.class,() -> new Labyrinthe(-8, 5));
+        assertThrows(IllegalArgumentException.class,() -> new Labyrinthe(8, -5));
+        assertThrows(IllegalArgumentException.class,() -> new Labyrinthe(-8, -5));
+        assertThrows(IllegalArgumentException.class,() -> new Labyrinthe(2, 2));
+    }
 
-	@Test
-	void testGetNbLignes() {
-		
-	}
+    @Test
+    void testGetNbColonnes() {
+        int[] RESULTAT = { 5, 8 };
+        for(int i = 0 ; i < correctes.size() ; i++) {
+            assertEquals(RESULTAT[i], correctes.get(i).getNbColonnes());
+        }
+    }
 
-	@Test
-	void testGetTableau() {
-		
-	}
+    @Test
+    void testGetNbLignes() {
+        int[] RESULTAT = { 3, 8 };
+        for(int i = 0 ; i < correctes.size() ; i++) {
+            assertEquals(RESULTAT[i], correctes.get(i).getNbLignes());
+        }
+    }
 
-	@Test
-	void testHashCode() {
-		
-	}
+    @Test
+    void testGetTableau() {
+        int[][] RESULTAT = {
+                {1, 2, 3},
+                {4, 5, 6},
+                {7, 8, 9}
+        };
+        assertArrayEquals(RESULTAT, new Labyrinthe(3, 3).getTableau());
+    }
+
+    @Test
+    void testHashCode() {
+        assertEquals(correctes.get(0).hashCode(), correctes.get(0).hashCode());
+        assertEquals(correctes.get(1).hashCode(), correctes.get(1).hashCode());
+
+    }
+
+    @Test
+    void testToString() {
+    
+    }
 
 }
