a964a31dfdcba0aad604d88ed15709b272432a76
Hugues1702 <120390285+Hugues1702@users.noreply.github.com>
2023-05-25 09:09:05 +0200

    Add files via upload

--- src/iut/info1/labyrinthe/EcrireLeLabyrinthe.java
+++ src/iut/info1/labyrinthe/EcrireLeLabyrinthe.java
@@ -4,13 +4,14 @@
  */
 package iut.info1.labyrinthe;
 
-
+import static iut.info1.labyrinthe.controle.Controleur.labyrintheActuel;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
 /** TODO comment class responsibility (SRP)
  * @author hugues.bertrand
  *
@@ -26,22 +27,23 @@ public class EcrireLeLabyrinthe {
      * @throws IOException 
      */
     public static void main(String[] args) throws IOException {
-      
+        
+    }
+        
+        
+    
+    /** TODO comment method role
+     * @throws IOException 
+     * 
+     */
+    public static void ecrireDansLeFichier() throws IOException {
+        
         String fichierTxt = "Z:\\BUT_1\\Semestre_2\\SAE ALGO\\Workspace sae"
-                       + "\\Labyrinthe\\src\\iut\\info1\\labyrinthe\\idk.txt",
-               contenu,
-               valeursRecuperees;
-        int nbLignes,
-            nbColonnes;
-        String[] valeursFinales;
+                + "\\Labyrinthe\\src\\iut\\info1\\labyrinthe\\informations_labyrinthe.txt",
+               contenu;
         
-        Labyrinthe essai = new Labyrinthe(5,10);
+        Labyrinthe essai = labyrintheActuel;
         
-        /*
-         * On récupère les informations du labyrinthe grace à la méthode getInfoTxt()
-         * que nous avons développée dans la classe Labyrinthe
-         */
-        contenu = essai.getInfoTxt();
         
         /*
          * On cherche le fichier dans le package voulu et on en créé un objet
@@ -53,7 +55,11 @@ public class EcrireLeLabyrinthe {
         if (!file.exists()) {
             file.createNewFile();
         }
-   
+        
+        
+        /* OutputStream permet d'écrire dans le fichier séléctionné
+         * (celui-ci doit être contenu dans un Objet de type File 
+         */
         FileOutputStream fos = null; // stub
         try {
             fos = new FileOutputStream(file);
@@ -62,38 +68,55 @@ public class EcrireLeLabyrinthe {
             e.printStackTrace();
         }
         
+        /*
+         * On récupère les informations du labyrinthe grace à la méthode getInfoTxt()
+         * que nous avons développée dans la classe Labyrinthe
+         */
+        contenu = essai.getInfoTxt();
+        
         //convertion en bytes car la méthode write() ne fonctionne qu'avec des bytes.
         byte[] contenuEnBytes = contenu.getBytes();
         
         // écriture des informations dans le fichier
         fos.write(contenuEnBytes);
         
-        /* Maintenant on doit récupérer les données à partir du fichier pour
-         * les renseigner dans les int.
-         * Pour ceci on a besoin d'un FileInputStream.
-         */
-        
-        //
-        
-        FileInputStream fis = new FileInputStream(fichierTxt);   
+    }
     
-        valeursRecuperees = ""+fis.read(); 
+    /** TODO comment method role
+     * @throws IOException
+     */
+    public static void recupererInfosFichier() throws IOException {
+        String fichierTxt = "Z:\\BUT_1\\Semestre_2\\SAE ALGO\\Workspace sae"
+                + "\\Labyrinthe\\src\\iut\\info1\\labyrinthe\\idk.txt",
+                valeursRecuperees;
         
+        int nbLignes,
+            nbColonnes;
+        String[] valeursFinales;
+      
+        //On cherche le fichier dans le package voulu et on en créé un objet    
+        FileInputStream fis = new FileInputStream(fichierTxt);   
+        // Avec l'objet InputStream On créé un objet StreamReader pour être
+        // capable de lire les caractères du fichier et non les bytes
+        InputStreamReader ipsr = new InputStreamReader(fis);
+        // Puis finalement grâce à un objet BufferedReader, on peut lire
+        // selon différents facteurs. Ici on lit selon la ligne
+        BufferedReader br = new BufferedReader(ipsr);
         
-        System.out.println(valeursRecuperees);
-        
-        /*
+        valeursRecuperees = br.readLine();
+        br.close();
+
         valeursFinales = valeursRecuperees.split(";");
         
         nbLignes = Integer.parseInt(valeursFinales[0]);
         nbColonnes = Integer.parseInt(valeursFinales[1]);
         
-        System.out.println(nbLignes +"" + nbColonnes); */
-                
+        System.out.printf("lignes : %d    colonnes : %d", nbLignes, nbColonnes);        
         
+
     }
+
     
     
 }
 
-// NIQUE TA PUTE DE MERE
