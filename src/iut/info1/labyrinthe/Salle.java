ec5564f370241b758901112fd71ed155a53d79a8
djedline <103513050+djedline@users.noreply.github.com>
2023-05-25 08:14:56 +0200

    Ajout javadoc salle

--- src/iut/info1/labyrinthe/Salle.java
+++ src/iut/info1/labyrinthe/Salle.java
@@ -1,5 +1,10 @@
 package iut.info1.labyrinthe;
 
+/** 
+ * Permet de modéliser une pièce d'un labyrinthe
+ * @author djedline.boyer
+ *
+ */
 public class Salle {
 	
 	int index;
@@ -7,14 +12,26 @@ public class Salle {
 	boolean porteOuest;
 	int marque;
 	
+	/** 
+	 * Récupère la marque de la salle
+	 * @return la marque récupéré
+	 */
 	public int getMarque() {
 		return marque;
 	}
 
+	/** 
+	 * Modifie la marque de la salle
+	 * @param marque la marque à modifier
+	 */
 	public void setMarque(int marque) {
 		this.marque = marque;
 	}
 
+	/** 
+	 * Initialise une nouvelle salle
+	 * @param index
+	 */
 	public Salle(int index) {
 		super();
 		if (!isValide(index)) {
@@ -27,22 +44,40 @@ public class Salle {
 		return index >= 0;
 	}
 
+	/** 
+	 * 
+	 * @return true si la porteNord existe
+	 *         false sinon
+	 */
 	public boolean isPorteNord() {
 		return porteNord;
 	}
 
+	/** TODO method field role
+	 * @param porteNord
+	 */
 	public void setPorteNord(boolean porteNord) {
 		this.porteNord = porteNord;
 	}
 
+	/** TODO method field role
+	 * @return true si la porte Ouest existe
+	 *         false sinon
+	 */
 	public boolean isPorteOuest() {
 		return porteOuest;
 	}
 
+	/** TODO method field role
+	 * @param porteOuest
+	 */
 	public void setPorteOuest(boolean porteOuest) {
 		this.porteOuest = porteOuest;
 	}
 
+	/** TODO method field role
+	 * @return l'index
+	 */
 	public int getIndex() {
 		return index;
 	}
