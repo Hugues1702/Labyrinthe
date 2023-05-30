6da70bbe702192d7d64bbe173d2ddb2fec3b20bf
djedline <103513050+djedline@users.noreply.github.com>
2023-05-30 17:29:42 +0200

    ecrireFichier

--- src/iut/info1/labyrinthe/controle/ControleurFichier.java
+++ src/iut/info1/labyrinthe/controle/ControleurFichier.java
@@ -4,13 +4,15 @@
  */
 package iut.info1.labyrinthe.controle;
 
+import static iut.info1.labyrinthe.controle.Controleur.fichierLabyrintheActuel;
+import static iut.info1.labyrinthe.controle.Controleur.labyrintheActuel;
+
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import iut.info1.labyrinthe.Labyrinthe;
-import static iut.info1.labyrinthe.controle.Controleur.labyrintheActuel;
 
 
 /** 
@@ -22,7 +24,7 @@ public class ControleurFichier {
 	
 	private final static String BASE_NOM = "src\\SauvegardeLabyrinthe\\Labyrinthe";
 	private final static char SEPARATEUR = '_';
-	private static File file;
+	//private static File file;
 	
 	/** TODO method field role
 	 * @return x
@@ -31,10 +33,14 @@ public class ControleurFichier {
 		File monRepertoire = new File("SauvegardeLabyrinthe");
 		File[] listeFichier = monRepertoire.listFiles();
 		int nombreFichier = 0;
-		for (int i = 0 ; i < listeFichier.length ; i++) {
-		  if (listeFichier[i].isFile()) {
-			  nombreFichier++;
-		  }
+		if (listeFichier == null) {
+			nombreFichier = 0;
+		} else {
+			for (int i = 0 ; i < listeFichier.length ; i++) {
+				if (listeFichier[i].isFile()) {
+					nombreFichier++;
+				}
+			}
 		}
 		return nombreFichier; 
 	}
@@ -48,91 +54,45 @@ public class ControleurFichier {
 				  + "L" + labyrintheActuel.getNbLignes() + SEPARATEUR 
 				  + "C" + labyrintheActuel.getNbColonnes()+ SEPARATEUR 
 				  + nombreFichierSauvegarde();
-		System.out.println(nomFichier);
-		/*file = new File(nomFichier);
+		fichierLabyrintheActuel = new File(nomFichier);
 		// si ce fichier n'existe pas, alors on le créé.
-		if (!file.exists()) {
-			file.createNewFile();
-			System.out.println("fichier cree");
-		}*/
+		if (!fichierLabyrintheActuel.exists()) {
+			fichierLabyrintheActuel.createNewFile();
+			System.out.println(nomFichier + " est cree.");
+		}
 	}
 	
-	/** TODO method field role
+	/** 
+	 * Permet de récupérer un fichier et d'écrire
 	 * @throws IOException 
-	 * 
 	 */
-	public void stockage() throws IOException {
-		String fichierTxt = "src\\iut\\info1\\labyrinthe\\idk.txt",
-				contenu,
-				valeursRecuperees;
-		int nbLignes,
-		nbColonnes;
-		String[] valeursFinales;
+	public static void ecritureFichier() throws IOException {
+		String fichierTxt = "Z:\\BUT_1\\Semestre_2\\SAE ALGO\\Workspace sae"
+				+ "\\Labyrinthe\\src\\iut\\info1\\labyrinthe\\informations_labyrinthe.txt",
+				contenu;
 
-		Labyrinthe essai = new Labyrinthe(5,10);
-
-		/*
-		 * On récupère les informations du labyrinthe grace à la méthode getInfoTxt()
-		 * que nous avons développée dans la classe Labyrinthe
+		/* OutputStream permet d'écrire dans le fichier séléctionné
+		 * (celui-ci doit être contenu dans un Objet de type File 
 		 */
-		contenu = essai.getNbLignes() + ";" + essai.getNbColonnes() ;
-
-		/*
-		 * On cherche le fichier dans le package voulu et on en créé un objet
-		 * sur lequel on va pouvoir intéragir avec
-		 */ 
-		File file = new File(fichierTxt);
-
-		// si ce fichier n'existe pas, alors on le créé.
-		if (!file.exists()) {
-			file.createNewFile();
-		}
-
 		FileOutputStream fos = null; // stub
 		try {
-			fos = new FileOutputStream(file);
+			fos = new FileOutputStream(fichierLabyrintheActuel);
 		} catch (FileNotFoundException e) {
 			// Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		/*FileReader fr = new FileReader(fichierLabyrintheActuel);
+		int nbBoucle = ;*/
+		//Récupération des informations du labyrinthe
+		contenu = labyrintheActuel.getNbLignes() + ";" + labyrintheActuel.getNbColonnes();
 
 		//convertion en bytes car la méthode write() ne fonctionne qu'avec des bytes.
 		byte[] contenuEnBytes = contenu.getBytes();
 
 		// écriture des informations dans le fichier
 		fos.write(contenuEnBytes);
-
-		/* Maintenant on doit récupérer les données à partir du fichier pour
-		 * les renseigner dans les int.
-		 * Pour ceci on a besoin d'un FileInputStream.
-		 */
-
-		//
-
-		FileInputStream fis = new FileInputStream(fichierTxt);   
-
-		valeursRecuperees = ""+fis.read(); 
-
-
-		System.out.println(valeursRecuperees);
-
-		/*
-	        valeursFinales = valeursRecuperees.split(";");
-
-	        nbLignes = Integer.parseInt(valeursFinales[0]);
-	        nbColonnes = Integer.parseInt(valeursFinales[1]);
-
-	        System.out.println(nbLignes +"" + nbColonnes); */
-
-
-	}
-
-	/** TODO comment method role
-	 * @param args
-	 * @throws IOException 
-	 */
-	public static void main(String[] args) throws IOException {
-		nouveauFichier();
 	}
 }
-	// NIQUE TA PUTE DE MERE
+	
+	
