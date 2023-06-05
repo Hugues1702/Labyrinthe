16a6717e5af28b7ae536ddb6c3b23c579973fab7
Hugues1702 <120390285+Hugues1702@users.noreply.github.com>
2023-06-05 14:26:57 +0200

    Add files via upload
    =)

--- src/iut/info1/labyrinthe/controle/ControleurFichier.java
+++ src/iut/info1/labyrinthe/controle/ControleurFichier.java
@@ -9,13 +9,16 @@ import static iut.info1.labyrinthe.controle.Controleur.labyrintheActuel;
 import static iut.info1.labyrinthe.controle.Controleur.nbColonnes;
 import static iut.info1.labyrinthe.controle.Controleur.nbLignes;
 
-import java.io.BufferedReader;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+import iut.info1.labyrinthe.Labyrinthe;
 
 
 /** 
@@ -27,8 +30,6 @@ public class ControleurFichier {
         
         private final static String BASE_NOM = "src\\SauvegardeLabyrinthe\\Labyrinthe";
         private final static char SEPARATEUR = '_';
-        private static String valeursRecuperees;
-        //private static File file;
         
         /** TODO method field role
          * @return x
@@ -70,70 +71,44 @@ public class ControleurFichier {
          * Permet de récupérer un fichier et d'écrire
          * @throws IOException 
          */
-        public static void ecritureFichier() throws IOException {
-               // String fichierTxt = "Z:\\BUT_1\\Semestre_2\\SAE ALGO\\Workspace sae"
-              //                  + "\\Labyrinthe\\src\\iut\\info1\\labyrinthe\\informations_labyrinthe.txt",
-              //                  contenu;
+        public static void ecritureFichier() {
+                
+            try {
+                ObjectOutputStream os = new ObjectOutputStream(new FileOutputStream(fichierLabyrintheActuel));
+                os.writeObject(labyrintheActuel);
+                os.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } 
 
-                /* OutputStream permet d'écrire dans le fichier séléctionné
-                 * (celui-ci doit être contenu dans un Objet de type File 
-                 */
-                FileOutputStream fos = null; // stub
-                try {
-                        fos = new FileOutputStream(fichierLabyrintheActuel);
-                } catch (FileNotFoundException e) {
-                        // Auto-generated catch block
-                        e.printStackTrace();
-                }
-
-                /*FileReader fr = new FileReader(fichierLabyrintheActuel);
-                int nbBoucle = ;*/
-                String contenu;
-                int nbSalle = nbLignes * nbColonnes;
-                //Récupération des informations du labyrinthe
-                contenu = "";
-                contenu += labyrintheActuel.hashCode() + "\n";
-                for (int ligne = 0; ligne < nbLignes; ligne++) {
-                    for (int colonne = 0; colonne < nbColonnes; colonne++) {
-                        contenu += labyrintheActuel.getSalle(ligne, colonne).hashCode() + "\n";
-                        System.out.println(contenu);
-                    }
-                }
-
-                //convertion en bytes car la méthode write() ne fonctionne qu'avec des bytes.
-                byte[] contenuEnBytes = contenu.getBytes();
-
-                // écriture des informations dans le fichier
-                fos.write(contenuEnBytes);
-                //System.out.println(fos);
         }
         
         /** 
          * Recupère les données sauvegarder dans un fichier
          * @throws IOException
          */
-        public static void RecupDonnee() throws IOException {
+        public static void RecupDonnee() {
             
-            String[] valeursFinales;
-          
-            //On cherche le fichier dans le package voulu et on en créé un objet    
-            FileInputStream fis = new FileInputStream(fichierLabyrintheActuel);   
-            // Avec l'objet InputStream On créé un objet StreamReader pour être
-            // capable de lire les caractères du fichier et non les bytes
-            InputStreamReader ipsr = new InputStreamReader(fis);
-            // Puis finalement grâce à un objet BufferedReader, on peut lire
-            // selon différents facteurs. Ici on lit selon la ligne
-            BufferedReader br = new BufferedReader(ipsr);
+            try {
+                ObjectInputStream is = new ObjectInputStream(new FileInputStream(fichierLabyrintheActuel));
+                Labyrinthe laby = (Labyrinthe) is.readObject();
+                System.out.println(laby);
+                is.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } 
             
-            valeursRecuperees = br.readLine();
-            br.close();
-            System.out.println(valeursRecuperees);
-            //valeursFinales = valeursRecuperees.split(";");
-            
+        }
             
 
         
-        }
 }
+
         
         
