3e774569dcd44f609fff3fe517564d6930e9e9f6
djedline <103513050+djedline@users.noreply.github.com>
2023-06-05 19:13:40 +0200

    modification_test

--- src/iut/info1/labyrinthe/tests/TestAlgo1.java
+++ src/iut/info1/labyrinthe/tests/TestAlgo1.java
@@ -4,19 +4,19 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import iut.info1.labyrinthe.Labyrinthe;
+import iut.info1.labyrinthe.parcours.ParcoursProfondeur;
 
 class TestAlgo1 {
 
-	@BeforeEach
-	void setUp() throws Exception {
-	}
+        @BeforeEach
+        void setUp() throws Exception {
+        }
 
-	@Test
-	void testLabyrinthe() {
-		new Labyrinthe(3,3);
-		new Labyrinthe(4,4);
-		new Labyrinthe(5,5);
-		new Labyrinthe(6,6);
-	}
+        @Test
+        void testLabyrinthe() {
+                Labyrinthe test = new Labyrinthe(4,4);
+                System.out.println(test);
+                ParcoursProfondeur.parcours(test);
+        }
 
 }
--- src/iut/info1/labyrinthe/tests/TestSalle.java
+++ src/iut/info1/labyrinthe/tests/TestSalle.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -12,89 +11,89 @@ import org.junit.jupiter.api.Test;
 import iut.info1.labyrinthe.Salle;
 
 class TestSalle {
-	
-	Salle NO;
-	Salle NE;
-	Salle SO;
-	Salle SE;
+        
+        Salle NO;
+        Salle NE;
+        Salle SO;
+        Salle SE;
 
-	@BeforeEach
-	void setUp() throws Exception {
-		NO = new Salle(0);
-		NE = new Salle(1);
-		SO = new Salle(2);
-		SE = new Salle(3);
-	}
+        @BeforeEach
+        void setUp() throws Exception {
+                NO = new Salle(0);
+                NE = new Salle(1);
+                SO = new Salle(2);
+                SE = new Salle(3);
+        }
 
-	@Test
-	void testSalle() {
-		assertThrows(IllegalArgumentException.class, () -> {
-			new Salle(-1);
-		});
-		assertThrows(IllegalArgumentException.class, () -> {
-			new Salle(Integer.MIN_VALUE);
-		});
-		assertThrows(IllegalArgumentException.class, () -> {
-			new Salle(-3000);
-		});
-	}
+        @Test
+        void testSalle() {
+                assertThrows(IllegalArgumentException.class, () -> {
+                        new Salle(-1);
+                });
+                assertThrows(IllegalArgumentException.class, () -> {
+                        new Salle(Integer.MIN_VALUE);
+                });
+                assertThrows(IllegalArgumentException.class, () -> {
+                        new Salle(-3000);
+                });
+        }
 
-	@Test
-	void testIsPorteNord() {
-		assertFalse(NO.isPorteNord());
-	}
+        @Test
+        void testIsPorteNord() {
+                assertFalse(NO.isPorteNord());
+        }
 
-	@Test
-	void testSetPorteNord() {
-		NO.setPorteNord(true);
-		assertTrue(NO.isPorteNord());
-		NO.setPorteNord(false);
-		assertFalse(NO.isPorteNord());
-	}
+        @Test
+        void testSetPorteNord() {
+                NO.setPorteNord(true);
+                assertTrue(NO.isPorteNord());
+                NO.setPorteNord(false);
+                assertFalse(NO.isPorteNord());
+        }
 
-	@Test
-	void testIsPorteOuest() {
-		assertFalse(NO.isPorteOuest());
-	}
+        @Test
+        void testIsPorteOuest() {
+                assertFalse(NO.isPorteOuest());
+        }
 
-	@Test
-	void testSetPorteOuest() {
-		NO.setPorteOuest(true);
-		assertTrue(NO.isPorteOuest());
-		NO.setPorteOuest(false);
-		assertFalse(NO.isPorteOuest());
-	}
+        @Test
+        void testSetPorteOuest() {
+                NO.setPorteOuest(true);
+                assertTrue(NO.isPorteOuest());
+                NO.setPorteOuest(false);
+                assertFalse(NO.isPorteOuest());
+        }
 
-	@Test
-	void testGetIndex() {
-		assertEquals(0, NO.getIndex());
-		assertEquals(1, NE.getIndex());
-		assertEquals(2, SO.getIndex());
-		assertEquals(3, SE.getIndex());
-	}
+        @Test
+        void testGetIndex() {
+                assertEquals(0, NO.getIndex());
+                assertEquals(1, NE.getIndex());
+                assertEquals(2, SO.getIndex());
+                assertEquals(3, SE.getIndex());
+        }
 
-	@Test
-	void testHashCode() {
-		assertEquals(NO.hashCode(), NO.hashCode());
-		assertEquals(NE.hashCode(), NE.hashCode());
-		assertEquals(SO.hashCode(), SO.hashCode());
-		assertEquals(SE.hashCode(), SE.hashCode());
-	}
+        @Test
+        void testHashCode() {
+                assertEquals(NO.hashCode(), NO.hashCode());
+                assertEquals(NE.hashCode(), NE.hashCode());
+                assertEquals(SO.hashCode(), SO.hashCode());
+                assertEquals(SE.hashCode(), SE.hashCode());
+        }
 
-	@Test
-	void testEqualsObject() {
-		assertTrue(NO.equals(NO));
-		assertFalse(NO.equals(NE));
-	}
-	
-	@Test
-	void testGetMarque() {
-		assertEquals(0, NO.getMarque());
-	}
-	
-	@Test
-	void testSetMarque() {
-		NO.setMarque(1);
-		assertEquals(1, NO.getMarque());
-	}
+        @Test
+        void testEqualsObject() {
+                assertTrue(NO.equals(NO));
+                assertFalse(NO.equals(NE));
+        }
+        
+        @Test
+        void testGetMarque() {
+                assertEquals(0, NO.getMarque());
+        }
+        
+        @Test
+        void testSetMarque() {
+                NO.setMarque(1);
+                assertEquals(1, NO.getMarque());
+        }
 }
