4937df1c101c92d98b62e2bf92f252ced02242a5
djedline <103513050+djedline@users.noreply.github.com>
2023-06-05 19:15:44 +0200

    Pile

---
+++ src/iut/info1/sdd/PileVideException.java
@@ -0,0 +1,23 @@
+package iut.info1.sdd;
+
+/**
+ * Exception levée en cas de pile vide.
+ */
+public class PileVideException extends RuntimeException {
+        
+        /** TODO comment initial state
+         * 
+         */
+        public PileVideException() {
+                super();
+        }
+
+        /** TODO comment initial state
+         * @param string
+         */
+        public PileVideException(String string) {
+                super(string);
+        }
+
+
+}
